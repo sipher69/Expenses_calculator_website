babcf6977173aafcd2e6f059a7135d674215ad08
sipher69 <jilanisaif2@gmail.com>
2022-05-03 03:58:15 +0300

    3/5/2022

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,7 @@
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
+
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    let title = props.title
+    const [title, setTitle] = useState(props.title);
+
 
     const clickHandler = () => {
-        title = 'Upadted'
+        setTitle('Updated')
         console.log(title);
-
     }
+
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+
             </div>
             <button onClick={clickHandler}> Change Title</button>
         </Card >
