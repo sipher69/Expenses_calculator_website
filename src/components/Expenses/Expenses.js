ae4c1f39bec7b916227cdf8a071bfaa513249e8c
sipher69 <jilanisaif2@gmail.com>
2022-05-05 20:54:22 +0300

    05-may-2022

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,11 +12,35 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    let expensesContent = <p> No Expenses Found</p>
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+
+        )
+
+        )
+    }
+
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                {expensesContent}
             </Card>
         </div>
     );
