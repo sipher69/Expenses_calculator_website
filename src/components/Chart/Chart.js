3580e8b0130d972322ee779b98b7892231b8008b
sipher69 <jilanisaif2@gmail.com>
2022-05-08 04:31:53 +0300

    08/05/2022(2)

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,13 +2,15 @@ import React from "react";
 import ChartBar from './ChartBar.js'
 import './Chart.css'
 const Chart = props => {
+    const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointsValues)
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => (
                 <ChartBar
                     key={dataPoint.label}
                     value={dataPoint.value}
-                    maxValue={null}
+                    maxValue={totalMaximum}
                     label={dataPoint.label}
                 />
             ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,21 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
-    const filterChangeHandler = selectedYear => {
+    const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
 
-    const filteredExpenses = props.items.filter(expense => {
+    const filteredExpenses = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-
-
-    let expensesContent = <p> No Expenses Found</p>
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-
-        )
-
-        )
-    }
 
     return (
         <div>
@@ -40,7 +24,8 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {expensesContent}
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
